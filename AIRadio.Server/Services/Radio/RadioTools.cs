8300b31fb70145f20a146c82e197d8469ab7ecbf
peter-gregory <talktopwg@gmail.com>
2026-09-20 08:50:27 -0400

    Complete parameterized radio tool metadata

--- AIRadio.Server/Services/Radio/RadioTools.cs
+++ AIRadio.Server/Services/Radio/RadioTools.cs
@@ -38,6 +38,8 @@ public sealed class RadioPlayTool : RadioToolBase
     public RadioPlayTool(IMpvManager mpv, IMpvState state) : base(mpv, state) { }
     public override string Name => "radioPlay";
     public override string Intent => "Play a radio station.";
+    public override bool HasParameters => true;
+    public override string GetLlmRequestTemplate() => "{tool:radioPlay,stationName=!required!}";
     public override string GetLlmInstructions() => """
 RADIO PLAY
 
@@ -184,6 +186,8 @@ public sealed class RadioVolumeTool : RadioToolBase
     public RadioVolumeTool(IMpvManager mpv, IMpvState state) : base(mpv, state) { }
     public override string Name => "radioVolume";
     public override string Intent => "Set the radio volume.";
+    public override bool HasParameters => true;
+    public override string GetLlmRequestTemplate() => "{tool:radioVolume,volume=!required!}";
     public override bool HasParameters => true;
     public override string GetLlmRequestTemplate() => "{tool:radioVolume,volume=!required!}";
     public override string GetLlmInstructions() => """
@@ -344,6 +348,8 @@ public sealed class RadioSearchTool : ITool
     public RadioSearchTool(IRadioSearchClient search) => _search = search;
     public string Name => "radioSearch";
     public string Intent => "Search for radio stations.";
+    public bool HasParameters => true;
+    public string GetLlmRequestTemplate() => "{tool:radioSearch,query=!required!}";
     public bool HasParameters => true;
     public string GetLlmRequestTemplate() => "{tool:radioSearch,query=!required!}";
     public string GetLlmInstructions() => """
