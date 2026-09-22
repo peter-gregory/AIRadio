94ebfcfc9c46f051016caf0747c3ba8eb9fab438
peter-gregory <talktopwg@gmail.com>
2026-09-22 14:42:20 -0400

    Split schedule actions and add human time parser

---
+++ AIRadio.Server/Models/Alarms/RecurrenceTimeRange.cs
@@ -0,0 +1,26 @@
+namespace AIRadio.Server.Models.Alarms;
+
+public sealed class RecurrenceTimeRange
+{
+    public SchedulePatternType Type { get; init; }
+    public DateOnly? StartDate { get; init; }
+    public DateOnly? EndDate { get; init; }
+    public TimeSpan? TimeOfDay { get; init; }
+    public DayOfWeek[] DaysOfWeek { get; init; } = [];
+    public int? Month { get; init; }
+    public int? DayOfMonth { get; init; }
+    public int? WeekOfMonth { get; init; }
+    public DayOfWeek? WeekdayOfMonth { get; init; }
+
+    public SchedulePattern ToSchedulePattern() => new()
+    {
+        Type = Type,
+        Date = StartDate,
+        TimeOfDay = TimeOfDay,
+        DaysOfWeek = [.. DaysOfWeek],
+        Month = Month,
+        DayOfMonth = DayOfMonth,
+        WeekOfMonth = WeekOfMonth,
+        WeekdayOfMonth = WeekdayOfMonth
+    };
+}
