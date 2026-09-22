0c4f6f7a3ccd49a0ea64434874c70f71e63b69d5
peter-gregory <talktopwg@gmail.com>
2026-09-22 14:46:52 -0400

    Register dedicated event creation tool

--- AIRadio.Server/Program.cs
+++ AIRadio.Server/Program.cs
@@ -81,7 +81,7 @@ builder.Services.AddSingleton<ITool, WifiStatusTool>();
 builder.Services.AddSingleton<ITool, WifiNetworksTool>();
 builder.Services.AddSingleton<ITool, WifiConnectTool>();
 builder.Services.AddSingleton<ITool, AlarmTool>();
-builder.Services.AddSingleton<ITool, ReminderTool>();
+builder.Services.AddSingleton<ITool, EventAddTool>();
 builder.Services.AddSingleton<ITool, ExclusionTool>();
 builder.Services.AddSingleton<ITool, ConversationTool>();
 
