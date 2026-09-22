b6b58c1c6a860c3b6069e339adb4be5b7b598cb8
peter-gregory <talktopwg@gmail.com>
2026-09-22 15:50:45 -0400

    Remove internal time from conversation tool result

--- AIRadio.Server/Services/Tools/ConversationTool.cs
+++ AIRadio.Server/Services/Tools/ConversationTool.cs
@@ -33,11 +33,9 @@ CONVERSATION RESPONSE
     {
         cancellationToken.ThrowIfCancellationRequested();
 
-        var localTime = DateTime.Now.ToString("h:mm tt", System.Globalization.CultureInfo.InvariantCulture);
-
         return Task.FromResult(
             ToolResult.Successful(
                 Name,
-                $"Conversation response requested. Current local time is {localTime}."));
+                "Conversation response requested."));
     }
 }
