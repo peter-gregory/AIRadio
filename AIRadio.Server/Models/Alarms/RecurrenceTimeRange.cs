5c6bcb25b57ce7d6874e610d3ac5adfb0ebeec44
peter-gregory <talktopwg@gmail.com>
2026-09-22 14:46:39 -0400

    Simplify recurrence range model

--- AIRadio.Server/Models/Alarms/RecurrenceTimeRange.cs
+++ AIRadio.Server/Models/Alarms/RecurrenceTimeRange.cs
@@ -3,24 +3,12 @@ namespace AIRadio.Server.Models.Alarms;
 public sealed class RecurrenceTimeRange
 {
     public SchedulePatternType Type { get; set; }
-    public DateOnly? StartDate { get; init; }
-    public DateOnly? EndDate { get; init; }
-    public TimeSpan? TimeOfDay { get; init; }
-    public DayOfWeek[] DaysOfWeek { get; init; } = [];
-    public int? Month { get; init; }
-    public int? DayOfMonth { get; init; }
-    public int? WeekOfMonth { get; init; }
-    public DayOfWeek? WeekdayOfMonth { get; init; }
-
-    public SchedulePattern ToSchedulePattern() => new()
-    {
-        Type = Type,
-        Date = StartDate,
-        TimeOfDay = TimeOfDay,
-        DaysOfWeek = [.. DaysOfWeek],
-        Month = Month,
-        DayOfMonth = DayOfMonth,
-        WeekOfMonth = WeekOfMonth,
-        WeekdayOfMonth = WeekdayOfMonth
-    };
+    public DateOnly? StartDate { get; set; }
+    public DateOnly? EndDate { get; set; }
+    public TimeSpan? TimeOfDay { get; set; }
+    public DayOfWeek[] DaysOfWeek { get; set; } = [];
+    public int? Month { get; set; }
+    public int? DayOfMonth { get; set; }
+    public int? WeekOfMonth { get; set; }
+    public DayOfWeek? WeekdayOfMonth { get; set; }
 }
